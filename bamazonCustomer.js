35b76cbf169a2062068feb61eb050e2483da9b21
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-04-23 11:21:05 -0700

    cleaned up code. ended connections. got back to command prompt.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,41 +1,34 @@
 var mysql = require("mysql");
+
 var inquirer = require("inquirer");
+
 require("console.table");
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "CuL@8Poos",
-  database: "bamazon"
-});
+displayMerchandise();
 
-connection.connect(function(err) {
-  if (err) throw err;
-  displayMerchandise();
-});
+function newConnection() {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "CuL@8Poos",
+    database: "bamazon"
+  });
+  return connection;
+}
 
 function displayMerchandise() {
-  connection.query(
-    "select item_id, product_name, price from products",
-    function(err, res) {
-      if (err) throw err;
+  var cn = newConnection();
+  cn.connect(function(err) {
+    if (err) throw err;
+    cn.query("select item_id, product_name, price from products", function(
+      err,
+      res
+    ) {
       console.table(res);
+      cn.end();
       promptId(res);
-
-      // for (var i = 0; i < res.length; i++) {
-      // console.log(
-      //   "ITEM ID: " +
-      //     res[i].item_id +
-      //     " | " +
-      //     "ITEM NAME: " +
-      //     res[i].product_name +
-      //     " | " +
-      //     "ITEM PRICE: " +
-      //     res[i].price
-      // );
-    }
-  );
+    });
+  });
 }
 
 function promptId(inventory) {
@@ -54,72 +47,49 @@ function promptId(inventory) {
       }
     ])
     .then(function(processOrder) {
-      var c2 = mysql.createConnection({
-        host: "127.0.0.1",
-        port: 3306,
-        user: "root",
-        password: "CuL@8Poos",
-        database: "bamazon"
-      });
-
+      var c2 = newConnection();
       c2.connect(function(err) {
         if (err) throw err;
-
         q1 =
-          "select stock_quantity, price from products where item_id=" +
+          "select stock_quantity, price from products where item_id='" +
           processOrder.id_choice +
-          ";";
-
+          "';";
         console.log("Query is:", q1);
 
-        c2.query(
-          q1,
-
-          function(err, res2) {
-            console.table(res2);
-
-            if (res2[0].stock_quantity >= processOrder.qty_choice) {
-              console.log(
-                "Congratulations!  Your purchase total is:",
-                processOrder.qty_choice * res2[0].price
-              );
-
-              updateInventory(
-                processOrder.id_choice,
-                res2[0].stock_quantity - processOrder.qty_choice
-              );
-            } else {
-              console.log("Insufficient inventory");
-            }
+        c2.query(q1, function(err, res2) {
+          console.table(res2);
+          c2.end();
+          if (res2[0].stock_quantity >= processOrder.qty_choice) {
+            console.log(
+              "Congratulations!  Your purchase total is:",
+              processOrder.qty_choice * res2[0].price
+            );
+            updateInventory(
+              processOrder.id_choice,
+              res2[0].stock_quantity - processOrder.qty_choice
+            );
+          } else {
+            console.log("Insufficient inventory");
           }
-        );
+        });
       });
     });
 }
 
 function updateInventory(id, qty) {
-  var c3 = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "CuL@8Poos",
-    database: "bamazon"
-  });
-
+  var c3 = newConnection();
   console.log("Updating inventory", id, qty);
-
   c3.connect(function(err) {
     if (err) throw err;
-
     c3.query(
       "update products set stock_quantity = " +
         qty +
-        " where item_id=" +
+        " where item_id='" +
         id +
-        ";",
-
+        "';",
       function(err, res3) {
         console.log("Inventory updated");
+        c3.end();
       }
     );
   });
